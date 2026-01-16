4a77cb7eda841b312ddb2aecf61205ac99aaab31
sasha-y-lab <204271078+sasha-y-lab@users.noreply.github.com>
2026-01-16 18:25:42 -0500

    Added personal section

--- src/components/personal.jsx
+++ src/components/personal.jsx
@@ -1,20 +1,130 @@
 // name, email, phonenumber
+
+import { useState } from 'react';
+
+
+ 
+function PersonalItem(props) {
+  return <li>{props.info}</li>
+}
+
+
+function List(props) {
+  return (
+    <ul>
+      {props.personalInfo.map((info) => {
+        return <PersonalItem key={info} info={info} />;
+      })}
+    </ul>
+  );
+}
+
+
+const personalForm = new FormData();
+
+  const personalValues = {
+    firstName: personalForm.get('firstName'),
+  lastName: personalForm.get('lastName'),
+  email: personalForm.get('email'),
+  phoneNumber: personalForm.get('phoneNumber'),
+
+}; 
+
+
+
+
 function Personal() {
+
+
+const personalInfo = ['First Name', 'Last Name', 'Email', 'Phone Number'];
+ 
+
+
+
+const personalInfoList = personalInfo.map((info) => <li key={info}>{info}</li>)
+
+
+
+ 
+
   return (
   
   <>
 
-const heading = (
+
 
     <header>
 <h1>Upload Your CV</h1>
 
     </header>
-);
+    
+    <h3>Personal Information</h3>
+
+    
+
+
+
+ {/* 
+<form>
+
+ 
+  <div id="personal-formbox">
+      <label>First Name 
+   
+    <input type="text" name="firstName" placeholder="Jane" />
+    </label>
+    
+
+    <label>Last Name 
+    <input type="text" name="lastName" placeholder="Doe" />
+    </label>
+    
+
+    <label>Email 
+    <input type="email" name="email" placeholder="jane@yahoo.com" />
+    </label>
+   
+
+    <label>Phone Number
+    <input type="number" name="phoneNumber" placeholder="000-000-0000" />
+</label>
+
+</div>
+  </form>
+  */}
+ 
+ <form>
+
+  <ul>
+
+    <div id="labels">
+
+<List personalInfo={personalInfoList} />
+</div>
+
+<div id="inputs">
+<input type="text" name="firstName" placeholder='Jane'>{personalValues.firstName}</input>
+<input type="text" name="lastName" placeholder='Doe' >{personalValues.lastName}</input>
+<input type="email" name="email" placeholder='you@you.com' >{personalValues.email}</input>
+<input type="text" name="phoneNumber" placeholder='416-111-1111' >{personalValues.phoneNumber}</input>
+</div>
+
+</ul> 
+
+</form>
+ 
+
+
+
+ 
+
+
 
 
  </>
   );
 }
+
+
 
 export default Personal;
