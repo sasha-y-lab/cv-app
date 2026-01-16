427bc3aa052f4dd06dec7ea1ceb5bced3097eeae
sasha-y-lab <204271078+sasha-y-lab@users.noreply.github.com>
2026-01-16 13:54:14 -0500

    Add files

---
+++ src/components/personal.jsx
@@ -0,0 +1,20 @@
+// name, email, phonenumber
+function Personal() {
+  return (
+  
+  <>
+
+const heading = (
+
+    <header>
+<h1>Upload Your CV</h1>
+
+    </header>
+);
+
+
+ </>
+  );
+}
+
+export default Personal;
